4e8346e43dda0d5d81a3ef9054790196296efbe8
Ahmed <sultan_902002@yahoo.com>
2020-03-22 11:03:17 -0600

    delete extra code and import the header component

--- src/App.js
+++ src/App.js
@@ -1,67 +1,11 @@
 import React from 'react';
-import {
-  Container,
-  Content,
-  Button,
-  ListItem,
-  Text,
-  Icon,
-  Left,
-  Body,
-  Right,
-  Switch,
-  Spinner,
-} from 'native-base';
+import {Container} from 'native-base';
 import AppHeader from './components/header';
 
 const HelloWorldApp = () => {
   return (
     <Container>
       <AppHeader />
-      <Content>
-        <ListItem icon>
-          <Left>
-            <Button style={{backgroundColor: '#FF9501'}}>
-              <Icon active name="airplane" />
-            </Button>
-          </Left>
-          <Body>
-            <Text>Airplane Mode</Text>
-          </Body>
-          <Right>
-            <Switch value={false} />
-          </Right>
-        </ListItem>
-        <ListItem icon>
-          <Left>
-            <Button style={{backgroundColor: '#007AFF'}}>
-              <Icon active name="wifi" />
-            </Button>
-          </Left>
-          <Body>
-            <Text>Wi-Fi</Text>
-          </Body>
-          <Right>
-            <Text>GeekyAnts</Text>
-            <Icon active name="arrow-forward" />
-          </Right>
-        </ListItem>
-        <ListItem icon>
-          <Left>
-            <Button style={{backgroundColor: '#007AFF'}}>
-              <Icon active name="bluetooth" />
-            </Button>
-          </Left>
-          <Body>
-            <Text>Bluetooth</Text>
-          </Body>
-          <Right>
-            <Text>On</Text>
-            <Icon active name="arrow-forward" />
-          </Right>
-        </ListItem>
-        <Spinner color="#f6f6" />
-      </Content>
     </Container>
   );
 };
