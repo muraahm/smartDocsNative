e5d800ea9699f6848294aca96c4dcdb16dd2ecde
Ahmed <sultan_902002@yahoo.com>
2020-03-21 18:57:20 -0600

    first commit

---
+++ src/components/header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Header, Left, Body, Right, Title} from 'native-base';
+
+const AppHeader = () => {
+  return (
+    <Header>
+      <Left />
+      <Body>
+        <Title>Header</Title>
+      </Body>
+      <Right />
+    </Header>
+  );
+};
+
+export default AppHeader;
