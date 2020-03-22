ef48d86e5248b03999f7b5cb7bf0e961915f7177
Ahmed <sultan_902002@yahoo.com>
2020-03-22 13:47:37 -0600

    build home component

---
+++ src/screens/home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+// import { Header, Left, Body, Right, Title, Button, Icon } from 'native-base';
+
+const Home = () => {
+  return (
+    <View>
+      <Text>Home Screen</Text>
+    </View>
+  );
+};
+
+export default Home;
