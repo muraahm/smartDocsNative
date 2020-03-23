68d066cb7b992d55cb3d8067a21c95c728e16891
Ahmed <sultan_902002@yahoo.com>
2020-03-22 20:25:11 -0600

    change header title prop to header

--- src/routes/homeStack.js
+++ src/routes/homeStack.js
@@ -1,14 +1,25 @@
+import React from 'react';
 import {createStackNavigator} from 'react-navigation-stack';
-import {createAppContainer} from 'react-navigation';
+import AppHeader from '../components/header';
 import Home from '../screens/home';
 
 const screens = {
   Home: {
     screen: Home,
+    navigationOptions: ({navigation}) => {
+      return {
+        header: () => <AppHeader title="Home" navigation={navigation} />,
+      };
+    },
   },
 };
 
 // home stack navigator screens
-const HomeStack = createStackNavigator(screens);
+const HomeStack = createStackNavigator(screens, {
+  defaultNavigationOptions: {
+    headerTintColor: '#444',
+    headerStyle: {backgroundColor: '#eee', height: 60},
+  },
+});
 
-export default createAppContainer(HomeStack);
+export default HomeStack;
