513127c3ec6409d08ed3e2bc859732d0963b1ff8
Ahmed <sultan_902002@yahoo.com>
2020-03-23 11:14:55 -0600

    conditionaly render Auth component and Navigator if user loggedin

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import Navigator from './routes/drawer';
-import {Container} from 'native-base';
+import { Container } from 'native-base';
+import Auth from './components/authentication';
+import AsyncStorage from '@react-native-community/async-storage';
 
-const HelloWorldApp = () => {
+// AsyncStorage.setItem('token', '123456');
+// const value = AsyncStorage.getItem('token');
+// console.log(value);
+
+const App = () => {
+  const loggedin = true;
   return (
     <Container>
-      <Navigator />
+      {!loggedin && <Auth />}
+      {loggedin && <Navigator />}
     </Container>
   );
 };
 
-export default HelloWorldApp;
+export default App;
