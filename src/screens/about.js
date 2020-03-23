9f04dd2e2efef20f504a1ef656c35b87b09365d9
Ahmed <sultan_902002@yahoo.com>
2020-03-22 20:26:43 -0600

    create aboute screen

---
+++ src/screens/about.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+// import { Header, Left, Body, Right, Title, Button, Icon } from 'native-base';
+
+const About = () => {
+  return (
+    <View>
+      <Text>About Screen</Text>
+    </View>
+  );
+};
+
+export default About;
