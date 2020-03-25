210f7656716d7814397e234d4d88853e929517b2
Ahmed <sultan_902002@yahoo.com>
2020-03-25 00:09:39 -0600

    add context provider to access state and state management functions

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Navigator from './routes/drawer';
 import { Container } from 'native-base';
 import Auth from './components/authentication';
+import AppContextProvider from './contexts/appContext';
 import AsyncStorage from '@react-native-community/async-storage';
 
 // AsyncStorage.setItem('token', '123456');
@@ -12,10 +13,12 @@ const loggedin = false;
 const App = () => {
 
   return (
+    <AppContextProvider>
     <Container>
       {!loggedin && <Auth />}
       {loggedin && <Navigator />}
     </Container>
+    </AppContextProvider>
   );
 };
 
