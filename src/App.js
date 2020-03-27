b438d57a1044ae8874abd3bc802947a0313d4fe8
Ahmed <sultan_902002@yahoo.com>
2020-03-27 15:03:36 -0600

    add auth context to handle authentications

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Container from './components/container'
 import AppContextProvider from './contexts/appContext';
+import AuthContextProvider from './contexts/authContext'
 
 const App = () => {
 
   return (
     <AppContextProvider>
-      <Container/>
+      <AuthContextProvider>
+        <Container />
+      </AuthContextProvider>
     </AppContextProvider>
   );
 };
