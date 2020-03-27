3af2fd2078c4b431f9997de5f565c5e7d4fadd86
Ahmed <sultan_902002@yahoo.com>
2020-03-26 22:17:24 -0600

    use app container that handle diff views

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import React from 'react';
-import Navigator from './routes/drawer';
-import { Container } from 'native-base';
-import Auth from './components/authentication';
+import Container from './components/container'
 import AppContextProvider from './contexts/appContext';
-import AsyncStorage from '@react-native-community/async-storage';
-
-// AsyncStorage.setItem('token', '123456');
-// const value = AsyncStorage.getItem('token');
-// console.log(value); 
-const loggedin = false;
 
 const App = () => {
 
   return (
     <AppContextProvider>
-    <Container>
-      {!loggedin && <Auth />}
-      {loggedin && <Navigator />}
-    </Container>
+      <Container/>
     </AppContextProvider>
   );
 };
