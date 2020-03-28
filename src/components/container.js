54d07337910b7930b4578d52b8fb65e194bf88c5
Ahmed <sultan_902002@yahoo.com>
2020-03-27 21:39:24 -0600

    import react-native-reanimated

--- src/components/container.js
+++ src/components/container.js
@@ -4,6 +4,24 @@ import { Container } from 'native-base';
 import Auth from '../components/authentication';
 import Loading from '../components/loading'
 import { AppContext } from '../contexts/appContext'
+import Animated, { Easing } from 'react-native-reanimated';
+
+const {
+  Value,
+  event,
+  block,
+  cond,
+  eq,
+  set,
+  Clock,
+  startClock,
+  stopClock,
+  debug,
+  timing,
+  clockRunning,
+  interpolate,
+  Extrapolate
+} = Animated;
 
 
 const AppContainer = () => {
