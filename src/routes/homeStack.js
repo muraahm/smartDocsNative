1dd89ff995ee3f532ac8525b5ae0536c6f43c1ba
Ahmed <sultan_902002@yahoo.com>
2020-04-02 21:29:37 -0600

    delete extra header styling

--- src/routes/homeStack.js
+++ src/routes/homeStack.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import {createStackNavigator} from 'react-navigation-stack';
+import { createStackNavigator } from 'react-navigation-stack';
 import AppHeader from '../components/header';
 import Home from '../screens/home';
 
 const screens = {
   Home: {
     screen: Home,
-    navigationOptions: ({navigation}) => {
+    navigationOptions: ({ navigation }) => {
       return {
         header: () => <AppHeader title="Home" navigation={navigation} />,
       };
@@ -18,7 +18,7 @@ const screens = {
 const HomeStack = createStackNavigator(screens, {
   defaultNavigationOptions: {
     headerTintColor: '#444',
-    headerStyle: {backgroundColor: '#eee', height: 60},
+    headerStyle: { backgroundColor: 'red', height: 60 },
   },
 });
 
