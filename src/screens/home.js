5f9b9d4b33ec9f6f32362bd856912975b82a297c
Ahmed <sultan_902002@yahoo.com>
2020-04-04 21:54:44 -0600

    add footer and start implement use categories view

--- src/screens/home.js
+++ src/screens/home.js
@@ -1,13 +1,75 @@
-import React from 'react';
-import {View, Text} from 'react-native';
-// import { Header, Left, Body, Right, Title, Button, Icon } from 'native-base';
+import React, { useContext } from 'react';
+import { Container, Content, Footer, FooterTab, Button, Text, Icon } from 'native-base';
+import { AppContext } from '../contexts/appContext/appCtx';
+import { StyleSheet, View } from 'react-native';
 
 const Home = () => {
+
+  const { state } = useContext(AppContext);
+
+  // const categories = state.userCategories;
+
+  const categories = [
+    { id: 1, name: 'Personal', accountant_company: 'company1' },
+    { id: 2, name: 'Busnisse', accountant_company: 'company2' }
+  ]
+  const categoryList = categories.map(category => {
+
+    return (
+
+      <Button
+        style={styles.category}
+        key={category.id}
+        id={category.id}
+        categoryname={category.name}
+        accountantcompany={category.accountant_company}
+        onPress={() => props.viewCategory(category, category.accountant_company)}
+      ><Text>{category.name}</Text></Button>
+
+    )
+  })
+
+
   return (
-    <View>
-      <Text>Home Screen</Text>
-    </View>
+    <Container>
+      <Content>
+        {categoryList}
+        <View style={styles.addCategory}>
+          <Icon name="add"  style={styles.addIcon}/>
+        </View>
+      </Content>
+      <Footer>
+        <FooterTab>
+          <Button active>
+            <Text>Categories</Text>
+          </Button>
+          <Button>
+            <Text>New Category</Text>
+          </Button>
+          <Button >
+            <Text>Upload</Text>
+          </Button>
+        </FooterTab>
+      </Footer>
+    </Container>
   );
 };
+
+const styles = StyleSheet.create({
+  category: {
+    marginTop: 20,
+    marginHorizontal: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  addCategory: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: 20,
+  },
+  addIcon: {
+    fontSize: 60
+  }
+});
 
 export default Home;
