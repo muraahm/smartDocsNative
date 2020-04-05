568b99f43d9027d68b939bc6d089764a9c896816
Ahmed <sultan_902002@yahoo.com>
2020-04-04 21:05:19 -0600

    use customDrawer component

--- src/routes/drawer.js
+++ src/routes/drawer.js
@@ -1,9 +1,32 @@
-import {createDrawerNavigator} from 'react-navigation-drawer';
-import {createAppContainer} from 'react-navigation';
+import { createDrawerNavigator } from 'react-navigation-drawer';
+import { createAppContainer } from 'react-navigation';
+import CustomDrawer from '../components/customDrawer';
 
 // stacks
 import HomeStack from './homeStack';
 import AboutStack from './aboutStack';
+
+const DrawerNavigatorConfig = {
+  intialRouteName: 'Home',
+  contentComponent: CustomDrawer,
+  contentOptions: {
+    activeTintColor: '#e91e63',
+    itemStyle: {
+      marginVertical: 5,
+    },
+    itemsContainerStyle: {
+      borderRadius: 20,
+      borderWidth: 1,
+      borderColor: 'black',
+      marginVertical: 3,
+      backgroundColor: 'white',
+    },
+    iconContainerStyle: {
+      opacity: 1,
+    },
+  },
+  drawerBackgroundColor: '#262A2C', // sets background color of drawer
+};
 
 // drawer navigation options
 const RootDrawerNavigator = createDrawerNavigator({
@@ -13,6 +36,6 @@ const RootDrawerNavigator = createDrawerNavigator({
   About: {
     screen: AboutStack,
   },
-});
+}, DrawerNavigatorConfig);
 
 export default createAppContainer(RootDrawerNavigator);
